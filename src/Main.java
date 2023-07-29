076c5110d9b23c2c605cb4e75503bcd8c84822c5
Egor Danilov <mellowd94@gmail.com>
2023-07-29 07:48:58 +0200

    add PrintMenu and inputBytePostNumber

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.io.IOException;
+
 public class Main {
 
   public static void main(String[] args) throws FileNotFoundException {
@@ -9,6 +9,44 @@ public class Main {
     Scanner scanner = new Scanner(new File("res/dict.txt"));
     String n = scanner.nextLine();
     System.out.println(" Our dictionary hat " + n + " words");
-    System.out.println("Input number of words:");
+    printMenu();
+    Scanner scanner1 = new Scanner(System.in);
+    byte choice = scanner1.nextByte();
+
+    switch (choice) {
+      case (1):
+        System.out.println("Input number of words:");
+        Byte m = inputBytePostNumber(); // ввод положительного числа
+        for (int i = 0; i < m; i++) {
+          System.out.println(scanner.nextLine());
+        }
+        break;
+      case (2):
+
+        break;
+      default:
+        System.out.println("Input 1 or 2");
+        break;
+    }
+
+
+  }
+
+  public static void printMenu() {
+    System.out.println("What do you want?");
+    System.out.println("1. Get the number of words");
+    System.out.println("2. Get a accurate word");
+    System.out.print("\n Input the number of menu point ");
+  }
+
+  public static Byte inputBytePostNumber() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println(" ");
+    byte n = scanner.nextByte();
+    while (n <= 0) {
+      System.out.println("Вы ввели отрицательное число. Введите число больше нуля:");
+      n = scanner.nextByte();
+    }
+    return n;
   }
 }
